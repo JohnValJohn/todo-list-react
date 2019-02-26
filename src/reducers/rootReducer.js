24bf85459b7e359a3044d6907a13865c67bf7cfe
JohnValJohn <username@users.noreply.github.com>
2019-02-26 15:45:26 +0100

    store cleaning: add switch case for reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,19 +3,20 @@ import * as types from "../actions/actionsTypes";
 const initialState = { todos: [] };
 
 function rootReducer(state = initialState, action) {
-  if (action.type === types.UPDATE_TODOS) {
-    const unmodifiedTodos = state.todos.filter(todo => {
-      return todo.id !== action.changedItem.id;
-    });
-    const changedItem = Object.assign({}, action.changedItem, { done: true });
-    const newTodos = [...unmodifiedTodos, changedItem];
-    return Object.assign({}, state, { todos: newTodos });
+  switch (action.type) {
+    case types.UPDATE_TODOS:
+      const unmodifiedTodos = state.todos.filter(todo => {
+        return todo.id !== action.changedItem.id;
+      });
+      const changedItem = Object.assign({}, action.changedItem, { done: true });
+      const newTodos = [...unmodifiedTodos, changedItem];
+      return Object.assign({}, state, { todos: newTodos });
+    case types.CREATE_TODOS:
+      const newState = Object.assign({}, state, { todos: action.todos });
+      return newState;
+    default:
+      return state;
   }
-  if (action.type === types.CREATE_TODOS) {
-    const newState = Object.assign({}, state, { todos: action.todos });
-    return newState;
-  }
-  return state;
 }
 
 export default rootReducer;
